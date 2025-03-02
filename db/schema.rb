7332c795191a3163996b5ba51389e803647b45a3
Hanz Samonte <hsamonte@academic.rrc.ca>
2025-03-02 17:34:46 -0600

    1.3.2 - Generated a Character model. - Fixed ERD image (Character table's 'description' > 'role' (string).

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_02_231718) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_02_233117) do
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
+    t.integer "manga_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manga_id"], name: "index_characters_on_manga_id"
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,4 +35,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_02_231718) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "characters", "mangas"
 end
