bdfd26b5da62339cbee4015f574e1c20f83fb371
Hanz Samonte <hsamonte@academic.rrc.ca>
2025-03-02 18:55:10 -0600

    1.6 - Added validations to Manga model.

--- app/models/manga.rb
+++ app/models/manga.rb
@@ -3,4 +3,9 @@ class Manga < ApplicationRecord
   has_many :authors
   has_many :manga_genres
   has_many :genres, through: :manga_genres
+
+  # Validations
+  validates :title, :synopsis, :chapters, :release_date, presence: true
+  validates :chapters, numericality: { only_integer: true }
+  validates :title, uniqueness: true
 end
