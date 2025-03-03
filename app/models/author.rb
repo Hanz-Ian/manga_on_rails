df6f8e4d98c897a9bca8c0d9b24ebb4990522b50
Hanz Samonte <hsamonte@academic.rrc.ca>
2025-03-03 08:36:11 -0600

    1.3.5.2 - Updated Author and Manga model to reflect the associations with the MangaAuthor model.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
-  belongs_to :manga
+  has_many :manga_authors
+  has_many :mangas, through: :manga_authors
 
   # Validations
   validates :name, presence: true, uniqueness: true
--- app/models/manga.rb
+++ app/models/manga.rb
@@ -1,8 +1,9 @@
 class Manga < ApplicationRecord
   has_many :characters
-  has_many :authors
   has_many :manga_genres
   has_many :genres, through: :manga_genres
+  has_many :manga_authors
+  has_many :authors, through: :manga_authors
 
   # Validations
   validates :title, :synopsis, :chapters, :release_date, presence: true
